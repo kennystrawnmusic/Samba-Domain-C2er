d80ecf794c4017977ca265aadcb67ea40860619d
Kenneth Strawn <kstrawn0@saddleback.edu>
2025-11-29 07:18:15 -0800

    More existence checks, this time with DNS

--- targetresolver.sh
+++ targetresolver.sh
@@ -5,9 +5,16 @@ remsuffix=$2
 localccachepath=$3
 recname=$4
 remote=$5
+localhostname=$(hostname -s)
 
-# Create zone on local DC pointing to remote domain
-sudo samba-tool dns zonecreate 127.0.0.1 $remdom.$remsuffix --use-kerberos=required --use-krb5-ccache=$localccachepath
+# Check if zone exists before creating
+samba-tool dns query --use-kerberos=required --use-krb5-ccache=$localccachepath $localhostname $remdom.$remsuffix @ NS &> /dev/null
+
+if [ $? -eq 255 ]
+then
+  # Create zone on local DC pointing to remote domain
+  sudo samba-tool dns zonecreate $localhostname $remdom.$remsuffix --use-kerberos=required --use-krb5-ccache=$localccachepath
+fi
 
 # Create A record pointing at specified IP
-sudo samba-tool dns add 127.0.0.1 $remdom.$remsuffix $recname A $remote --use-kerberos=required --use-krb5-ccache=$localccachepath
+sudo samba-tool dns add $localhostname $remdom.$remsuffix $recname A $remote --use-kerberos=required --use-krb5-ccache=$localccachepath
