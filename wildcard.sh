c9b58deb392356040c32c10f7cfe33d785b49969
Kenneth Strawn <kstrawn0@saddleback.edu>
2025-11-28 20:05:43 -0800

    Reuse scripts whenever possible

--- wildcard.sh
+++ wildcard.sh
@@ -15,5 +15,4 @@ localip=${10}
 sudo samba-tool domain trust create $remdom.$remsuffix --type=forest --direction=both -U $remdom.$remsuffix\\$remdomadmuser --use-krb5-ccache=$remccachepath --local-dc-username=$localdom.$localsuffix\\$localdomadmuser --local-dc-use-krb5-ccache=$localccachepath
 
 # Create wildcard record pointing all target subdomains at our network-facing local IP
-sudo samba-tool dns zonecreate 127.0.0.1 $remdom.$remsuffix --use-kerberos=required --use-krb5-ccache=$localccachepath
-sudo samba-tool dns add 127.0.0.1 $remdom.$remsuffix "*" A $localip --use-kerberos=required --use-krb5-ccache=$localccachepath
+./targetresolver.sh $remdom $remsuffix $localccachepath "*" $remote
