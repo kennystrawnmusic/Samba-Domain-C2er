abb9eab676b9ec3286b8e0c92c19c04e98c00e11
Kenneth Strawn <kstrawn0@saddleback.edu>
2025-11-28 18:23:59 -0800

    Fix documentation

--- revdns.sh
+++ revdns.sh
@@ -7,5 +7,8 @@ recname=$4
 remote=$5
 localip=$6
 
+# Create zone on remote pointing at us
 sudo samba-tool dns zonecreate $remote $remdom.$remsuffix --use-kerberos=required --use-krb5-ccache=$remccachepath
+
+# Create A record on remote zone pointing at us
 sudo samba-tool dns add $remote $remdom.$remsuffix $recname A $localip --use-kerberos=required --use-krb5-ccache=$remccachepath
--- targetresolver.sh
+++ targetresolver.sh
@@ -6,5 +6,8 @@ localccachepath=$3
 recname=$4
 remote=$5
 
+# Create zone on local DC pointing to remote domain
 sudo samba-tool dns zonecreate 127.0.0.1 $remdom.$remsuffix --use-kerberos=required --use-krb5-ccache=$localccachepath
+
+# Create A record pointing at remote IP
 sudo samba-tool dns add 127.0.0.1 $remdom.$remsuffix $recname A $remote --use-kerberos=required --use-krb5-ccache=$localccachepath
