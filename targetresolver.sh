4fac09fb4d8f08be05cdb73baa327c2f3e4ee13d
Kenneth Strawn <kstrawn0@saddleback.edu>
2025-11-28 18:28:08 -0800

    Fix documentation

--- targetresolver.sh
+++ targetresolver.sh
@@ -9,5 +9,5 @@ remote=$5
 # Create zone on local DC pointing to remote domain
 sudo samba-tool dns zonecreate 127.0.0.1 $remdom.$remsuffix --use-kerberos=required --use-krb5-ccache=$localccachepath
 
-# Create A record pointing at remote IP
+# Create A record pointing at specified IP
 sudo samba-tool dns add 127.0.0.1 $remdom.$remsuffix $recname A $remote --use-kerberos=required --use-krb5-ccache=$localccachepath
--- wildcard.sh
+++ wildcard.sh
@@ -7,10 +7,10 @@ remsuffix=$4
 remote=$5
 localccachepath=$6
 remccachepath=$7
-remoteip=$8
+localip=$8
 
 # Create trust
 sudo samba-tool domain trust create $remdom.$remsuffix --type=forest --direction=both -U $remdom.$remsuffix\\Administrator --use-krb5-ccache=$remccachepath --local-dc-username=$localdom.$localsuffix\\Administrator --local-dc-use-krb5-ccache=$localccachepath
 
-# Create wildcard record
-./revdns.sh $remdom $remsuffix $localccachepath "*" 127.0.0.1 $remoteip
+# Create wildcard record pointing at our network-facing local IP
+./targetresolver.sh $localdom $localsuffix $localccachepath "*" $localip
