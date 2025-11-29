898639a6b09d8c81761b3fddea330a9c3b1ef8bf
Kenneth Strawn <kstrawn0@saddleback.edu>
2025-11-28 18:35:23 -0800

    Fix documentation

--- wildcard.sh
+++ wildcard.sh
@@ -9,7 +9,7 @@ localccachepath=$6
 remccachepath=$7
 localip=$8
 
-# Create trust
+# Create bidirectional trust necessary to facilitate this kind of spoofing
 sudo samba-tool domain trust create $remdom.$remsuffix --type=forest --direction=both -U $remdom.$remsuffix\\Administrator --use-krb5-ccache=$remccachepath --local-dc-username=$localdom.$localsuffix\\Administrator --local-dc-use-krb5-ccache=$localccachepath
 
 # Create wildcard record pointing at our network-facing local IP
